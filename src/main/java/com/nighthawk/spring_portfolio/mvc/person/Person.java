0d46d15739d40676fa9163c0dbc920cb20d1f0f4
mnarayan1 <madhumitas.narayan@gmail.com>
2022-11-23 12:16:39 -0800

    add tester method

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -1,5 +1,6 @@
 package com.nighthawk.spring_portfolio.mvc.person;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
@@ -38,9 +39,9 @@ The last annotation connect to database
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@TypeDef(name="json", typeClass = JsonType.class)
+@TypeDef(name = "json", typeClass = JsonType.class)
 public class Person {
-    
+
     // automatic unique identifier for Person record
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -48,35 +49,35 @@ public class Person {
 
     // email, password, roles are key attributes to login and authentication
     @NotEmpty
-    @Size(min=5)
-    @Column(unique=true)
+    @Size(min = 5)
+    @Column(unique = true)
     @Email
     private String email;
 
     @NotEmpty
     private String password;
 
-    // @NonNull, etc placed in params of constructor: "@NonNull @Size(min = 2, max = 30, message = "Name (2 to 30 chars)") String name"
+    // @NonNull, etc placed in params of constructor: "@NonNull @Size(min = 2, max =
+    // 30, message = "Name (2 to 30 chars)") String name"
     @NonNull
     @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
     private String name;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
-    
 
-    /* HashMap is used to store JSON for daily "stats"
-    "stats": {
-        "2022-11-13": {
-            "calories": 2200,
-            "steps": 8000
-        }
-    }
-    */
-    @Type(type="json")
+    /*
+     * HashMap is used to store JSON for daily "stats"
+     * "stats": {
+     * "2022-11-13": {
+     * "calories": 2200,
+     * "steps": 8000
+     * }
+     * }
+     */
+    @Type(type = "json")
     @Column(columnDefinition = "jsonb")
-    private Map<String,Map<String, Object>> stats = new HashMap<>(); 
-    
+    private Map<String, Map<String, Object>> stats = new HashMap<>();
 
     // Constructor used when building object from an API
     public Person(String email, String password, String name, Date dob) {
@@ -90,8 +91,22 @@ public class Person {
     public int getAge() {
         if (this.dob != null) {
             LocalDate birthDay = this.dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-            return Period.between(birthDay, LocalDate.now()).getYears(); }
+            return Period.between(birthDay, LocalDate.now()).getYears();
+        }
         return -1;
     }
 
+    public static void main(String[] args) {
+        // test zero arg constructr
+        Person zeroArg = new Person();
+
+        // test constructor with arguments
+        Date date = new Date();
+        Person personWithArgs = new Person("test@test.com", "password", "args", date);
+
+        // test properties of zero arg constructor
+        System.out.println("age: " + zeroArg.getAge());
+        // test properties of argument constructor
+        System.out.println("age: " + personWithArgs.getAge());
+    }
 }
